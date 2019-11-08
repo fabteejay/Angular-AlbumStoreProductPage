36af632dce91778279180740a90064dfd4c0568b
FabTeeJay <fabamiseolatunji@gmail.com>
2019-11-08 19:25:19 +0100

    i made change on font  family

---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+import { Http, Response } from '@angular/http;'
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+    private_albumUrl = '../assets/album.json';
+
+  constructor(private_http: Http) { }
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json());
+  }
+}
