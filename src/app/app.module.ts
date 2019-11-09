58b6ea6b280ee6223df2046289b3cfb65942f496
FabTeeJay <fabamiseolatunji@gmail.com>
2019-11-09 15:49:30 +0100

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
-import { RouterModule, RouterStateSnapshot } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router'
 
 const appRoutes: Routes = [
   { path: 'products', component: ProductListComponent },
